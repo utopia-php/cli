11477dbba2eb39c3cf2784805c0f015e12473754
Eldad Fux <eldad.fux@gmail.com>
2021-01-26 18:21:12 +0200

    New match, getTasks, and getArgs methods on the CLI

--- tests/CLI/CLITest.php
+++ tests/CLI/CLITest.php
@@ -89,4 +89,94 @@ class CLITest extends TestCase
 
         $this->assertEquals('me@example.com-item1-item2', $result);
     }
+
+    public function testGetTasks()
+    {
+        $cli = new CLI(['test.php', 'build', '--email=me@example.com', '--list[]=item1', '--list[]=item2']); // Mock command request
+
+        $cli
+            ->task('build1')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $cli
+            ->task('build2')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $this->assertCount(2, $cli->getTasks());
+    }
+
+    public function testGetArgs()
+    {
+        $cli = new CLI(['test.php', 'build', '--email=me@example.com', '--list[]=item1', '--list[]=item2']); // Mock command request
+
+        $cli
+            ->task('build1')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $cli
+            ->task('build2')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $this->assertCount(2, $cli->getArgs());
+        $this->assertEquals(['email' => 'me@example.com', 'list' => ['item1', 'item2']], $cli->getArgs());
+    }
+
+    public function testMatch()
+    {
+        $cli = new CLI(['test.php', 'build2', '--email=me@example.com', '--list[]=item1', '--list[]=item2']); // Mock command request
+
+        $cli
+            ->task('build1')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $cli
+            ->task('build2')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $this->assertEquals('build2', $cli->match()->getName());
+
+        $cli = new CLI(['test.php', 'buildx', '--email=me@example.com', '--list[]=item1', '--list[]=item2']); // Mock command request
+
+        $cli
+            ->task('build1')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $cli
+            ->task('build2')
+            ->param('email', null, new Email(), 'Valid email address')
+            ->param('list', null, new ArrayList(new Text(256)), 'List of strings')
+            ->action(function ($email, $list) {
+                echo $email.'-'.implode('-', $list);
+            });
+
+        $this->assertEquals(null, $cli->match());
+    }
 }
