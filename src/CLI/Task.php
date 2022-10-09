de3908706b244c342a7226b330647b18e65b3041
Damodar Lohani <dlohani48@gmail.com>
2022-10-09 08:25:08 +0000

    labels now in hook

--- src/CLI/Task.php
+++ src/CLI/Task.php
@@ -13,14 +13,6 @@ class Task extends Hook
      */
     protected string $name = '';
 
-    /**
-     * Labels
-     *
-     * List of route label names
-     *
-     * @var array
-     */
-    protected array $labels = [];
 
     /**
      * Task constructor.
@@ -34,21 +26,6 @@ class Task extends Hook
     }
 
     /**
-     * Add Label
-     *
-     * @param string $key
-     * @param mixed $value
-     *
-     * @return $this
-     */
-    public function label(string $key, $value): self
-    {
-        $this->labels[$key] = $value;
-
-        return $this;
-    }
-
-    /**
      * Get Name
      *
      * @return string
@@ -56,19 +33,5 @@ class Task extends Hook
     public function getName(): string
     {
         return $this->name;
-    }
-
-    /**
-     * Get Label
-     *
-     * Return given label value or default value if label doesn't exists
-     *
-     * @param string $key
-     * @param mixed $default
-     * @return mixed
-     */
-    public function getLabel(string $key, $default): mixed
-    {
-        return (isset($this->labels[$key])) ? $this->labels[$key] : $default;
     }
 }
